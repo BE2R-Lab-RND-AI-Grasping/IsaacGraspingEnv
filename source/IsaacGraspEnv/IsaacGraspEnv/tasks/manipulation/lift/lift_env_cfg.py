25404847d47288d457657faf0459b0e3eda874f9
Huowl <kraken.mix@gmail.com>
2025-08-18 20:13:40 +0300

    fix

--- source/IsaacGraspEnv/IsaacGraspEnv/tasks/manipulation/lift/lift_env_cfg.py
+++ source/IsaacGraspEnv/IsaacGraspEnv/tasks/manipulation/lift/lift_env_cfg.py
@@ -57,17 +57,17 @@ class ObjectTableSceneCfg(InteractiveSceneCfg):
     #     spawn=UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/SeattleLabTable/table_instanceable.usd",
     #                      semantic_tags = [("class","table"), ("color", "gray")],),
     # )
-    # table = AssetBaseCfg(
-    #     prim_path="{ENV_REGEX_NS}/Table",
-    #     init_state=AssetBaseCfg.InitialStateCfg(pos=[0.6, 0, -0.1], rot=[1, 0, 0, 0.0]),
-    #     spawn=sim_utils.CuboidCfg(
-    #         size= [1.4, 0.8, 0.2],
-    #         # rigid_props=sim_utils.RigidBodyPropertiesCfg(),
-    #         # mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
-    #         collision_props=sim_utils.CollisionPropertiesCfg(),
-    #         visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.6, 0.6, 0.9), metallic=0.2),
-    #     ),
-    #     )
+    table = AssetBaseCfg(
+        prim_path="{ENV_REGEX_NS}/Table",
+        init_state=AssetBaseCfg.InitialStateCfg(pos=[0.6, 0, -0.1], rot=[1, 0, 0, 0.0]),
+        spawn=sim_utils.CuboidCfg(
+            size= [1.4, 0.8, 0.2],
+            # rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+            # mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
+            collision_props=sim_utils.CollisionPropertiesCfg(),
+            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.6, 0.6, 0.9), metallic=0.2),
+        ),
+        )
 
     # plane
     # plane = AssetBaseCfg(
