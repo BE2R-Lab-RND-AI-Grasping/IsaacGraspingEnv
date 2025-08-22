008b974e36c953dd4f5b5bc9f5fb94af4ea62cd2
Huowl <kraken.mix@gmail.com>
2025-08-22 11:35:10 +0300

    fix double params

--- source/IsaacGraspEnv/IsaacGraspEnv/tasks/manipulation/lift/lift_env_cfg.py
+++ source/IsaacGraspEnv/IsaacGraspEnv/tasks/manipulation/lift/lift_env_cfg.py
@@ -249,7 +249,7 @@ class RewardsCfg:
         # For power drills
         params=add_is_contact_param({"std": 0.04, "minimal_height": 0.13, "command_name": "object_pose"}),
         # for screwdrives
-        params=add_is_contact_param({"std": 0.04, "minimal_height": 0.025, "command_name": "object_pose"}),
+        # params=add_is_contact_param({"std": 0.04, "minimal_height": 0.025, "command_name": "object_pose"}),
         weight=1/30 * 3/5 * 5/3 /0.2, #1.0/0.2/10, 
     )
     
